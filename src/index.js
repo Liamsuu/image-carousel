69825f342332843c808fd5e713e44f19f65109c4
Liam <liamsuu14974@gmail.com>
2024-04-26 18:04:08 +0100

    Get image background change functionality working

--- src/index.js
+++ src/index.js
@@ -1,20 +1,86 @@
 import "./index.css";
+import sandImage from "./pexels-marcelo-gonzalez-1141370437-20923044.jpg";
+import happyLadyImage from "./pexels-olly-789822.jpg";
 
 function createSlidesSkeleton() {
-  /*
-    There will be a main slidesFrame that will hold the image, and three blocks inside it of the same height.
-    The top and bottom blocks will have a colour, and the middle will be transparent so the user can see the image behind it inside the frame.
-    */
-  const slidesFrame = document.createElement("div"); // this will hold whatever the image is.
-  const topBlock = document.createElement("div");
-  const imageViewBlock = document.createElement("div");
-  const bottomBlock = document.createElement("div");
-  slidesFrame.style.display = "flex";
-  topBlock.className = "carousel-sections";
-  imageViewBlock.className = "carousel-sections";
-  bottomBlock.className = "carousel-sections";
-  document.querySelectorAll(".carousel-sections").forEach((section) => {
-    const carouselSections = section;
-    carouselSections.style.flex = 1;
-  });
+  const containerFrame = document.createElement("div");
+  const topSection = document.createElement("div");
+  const currentImageIndex = document.createElement("div");
+  const changeImageArrowsHolder = document.createElement("div");
+
+  containerFrame.className = "containerFrame";
+  topSection.className = "carouselTop";
+  currentImageIndex.className = "carouselImageIndexHolder"; // holds the circles that show what image number is displaying in the index.
+
+  topSection.style.display = "flex";
+  topSection.style.flex = 4;
+  topSection.style.alignItems = "center";
+  topSection.appendChild(changeImageArrowsHolder);
+
+  changeImageArrowsHolder.style.width = "100%";
+  changeImageArrowsHolder.style.height = "15%";
+
+  containerFrame.style.height = "50vh";
+  containerFrame.style.width = "40vw";
+  containerFrame.style.display = "flex";
+  containerFrame.style.flexDirection = "column";
+  containerFrame.style.backgroundSize = "cover";
+
+  currentImageIndex.style.flex = 1;
+
+  containerFrame.append(topSection, currentImageIndex);
+  return containerFrame;
 }
+
+let currentlySelectedIndex = 0; // will store number of currently selected image's index
+
+function changeImage(carouselSkeleton, images) {
+  setTimeout(() => {
+    const carouselFrame = carouselSkeleton;
+    const numOfImages = images.length - 1;
+
+    if (currentlySelectedIndex === numOfImages) {
+      currentlySelectedIndex = 0;
+      carouselFrame.style.backgroundImage = `url(${images[currentlySelectedIndex].src})`;
+    } else {
+      currentlySelectedIndex += 1;
+      carouselFrame.style.backgroundImage = `url(${images[currentlySelectedIndex].src})`;
+    }
+
+    changeImage(carouselFrame, images);
+  }, 10000);
+}
+
+function carouselFunctionality(carouselSkeleton, images, elementToAppendTo) {
+  const carouselFrame = carouselSkeleton;
+
+  // set initial image
+  carouselFrame.style.backgroundImage = `url(${images[currentlySelectedIndex].src})`;
+  elementToAppendTo.appendChild(carouselFrame);
+  changeImage(carouselFrame, images);
+}
+
+export default function createCarousel(images, elementToAppendTo) {
+  // images is an array of images.
+  const carouselSkeleton = createSlidesSkeleton();
+
+  carouselFunctionality(carouselSkeleton, images, elementToAppendTo);
+}
+
+const lady = new Image();
+lady.src = happyLadyImage;
+const sandPic = new Image();
+sandPic.src = sandImage;
+
+const images = [lady, sandPic];
+
+const element = document.querySelector("div"); // just for testing, add ability to specify where to append in the future.
+
+createCarousel(images, element);
+
+/**
+ * just search inside the skeleton and add an event listener on the container to listen
+ * for how much time passed. and every x amount of seconds change the image to one of the ones in the image array thats passed in the parameter.
+ * after the image changes, the circles on the bottom should highlight, the one highlighted should be the circle
+ * of the same index as the image shown. If the arrow is clicked reset timer and go either one left in the index, or one right in the index.
+ */
